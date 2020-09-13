162233ca1f3e16e5154f5854c415c40f13ff5d90
dev-juri <ojurifemi132@gmail.com>
2020-09-13 20:16:58 +0100

    Create project

---
+++ app/src/main/java/com/oluwafemi/recurmsg/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oluwafemi.recurmsg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
