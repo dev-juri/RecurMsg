a1fab9c6dfb6a2b0bfa816e154f1c4fc70a3462d
dev-juri <ojurifemi132@gmail.com>
2020-09-14 19:37:17 +0100

    Linked pages

--- app/src/main/java/com/oluwafemi/recurmsg/MainActivity.kt
+++ app/src/main/java/com/oluwafemi/recurmsg/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.oluwafemi.recurmsg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.oluwafemi.recurmsg.databinding.ActivityMainBinding
+import com.oluwafemi.recurmsg.ui.MessageActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, MessageActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
