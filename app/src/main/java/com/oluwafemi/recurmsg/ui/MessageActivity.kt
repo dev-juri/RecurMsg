56f9f659fd8c31e8ebe6a4bc9baed53e28f981b2
dev-juri <ojurifemi132@gmail.com>
2020-09-16 14:28:25 +0100

    update

--- app/src/main/java/com/oluwafemi/recurmsg/ui/MessageActivity.kt
+++ app/src/main/java/com/oluwafemi/recurmsg/ui/MessageActivity.kt
@@ -1,19 +1,35 @@
 package com.oluwafemi.recurmsg.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.oluwafemi.recurmsg.R
+import com.oluwafemi.recurmsg.databinding.ActivityMessageBinding
 
 class MessageActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMessageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_message)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_message)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        val recipientNumber = binding.phoneNumber.editText?.text
+        val messageBody = binding.textMessage.editText?.text
+        val recurNumber = binding.times.text
+
+        binding.sendBtn.setOnClickListener {
+            if (!recipientNumber.isNullOrEmpty() && !messageBody.isNullOrEmpty() && !recurNumber.isNullOrEmpty()){
+                Log.i("MESSAGE_LOG", "You want to send '$messageBody' to $recipientNumber, $recurNumber times.")
+            } else {
+                Toast.makeText(applicationContext, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return true
+        return super.onSupportNavigateUp()
     }
 }
