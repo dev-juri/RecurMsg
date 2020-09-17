a4971b3fc612be7bbd152feaa5e36e9c3fad6879
dev-juri <ojurifemi132@gmail.com>
2020-09-17 12:35:09 +0100

    setup recyclerview

---
+++ app/src/main/java/com/oluwafemi/recurmsg/adapter/MessageAdapter.kt
@@ -0,0 +1,38 @@
+package com.oluwafemi.recurmsg.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.oluwafemi.recurmsg.databinding.MessageListBinding
+import com.oluwafemi.recurmsg.model.MessageProperty
+
+class MessageAdapter() :
+    ListAdapter<MessageProperty, MessageAdapter.MessageViewHolder>(DiffUtilCallback) {
+    companion object DiffUtilCallback : DiffUtil.ItemCallback<MessageProperty>() {
+        override fun areItemsTheSame(oldItem: MessageProperty, newItem: MessageProperty): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: MessageProperty, newItem: MessageProperty): Boolean {
+            return oldItem.id == oldItem.id
+        }
+    }
+
+    class MessageViewHolder(private val binding : MessageListBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(messageProperty: MessageProperty) {
+            binding.messageProp = messageProperty
+            binding.executePendingBindings()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
+        return MessageViewHolder(MessageListBinding.inflate(LayoutInflater.from(parent.context)))
+    }
+
+    override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
+        val message = getItem(position)
+        holder.bind(message)
+    }
+}
--- app/src/main/java/com/oluwafemi/recurmsg/model/MessageProperty.kt
+++ app/src/main/java/com/oluwafemi/recurmsg/model/MessageProperty.kt
@@ -4,11 +4,9 @@ data class MessageProperty (
     val id : Int,
     val messageBody : String,
     val recipient : String,
-    val dateSent : Long,
-    val timeSent : Long,
+    val dateAndTime : String,
     val totalSendTime : Int,
     val totalSuccessfullySent : Int
 ) {
     val status = "$totalSuccessfullySent of $totalSendTime sent."
-    val dateAndTime = "$timeSent, $dateSent"
 }
